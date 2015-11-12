4ffafe0debff6931721adddacadd8e277a75ef55
Lewis Kabui <lewis.maina@andela.com>
2015-11-12 15:40:43 +0300

    Checkpoint 1 submission review
    - PEP8 stying applied
    - Getters and setters removed
    - Gets file path dynamically for read and/or write operations.
    - Change term 'folder' to 'directory' in README.md file
    - Single backtick used for inline syntax styling
    - Removed spacing between number and menu item when accessing room occupants

--- people/staff.py
+++ people/staff.py
@@ -1,6 +1,4 @@
 import person
 
-# Staff cannot be allocated a living space
-# but can be allocated an office
 class Staff(person.Person):
 	pass
--- spaces/living.py
+++ spaces/living.py
@@ -2,18 +2,12 @@ from room import Room
 from custom.overflow import OverflowException
 from people.fellow import Fellow
 
-# Living spaces have a default maximum of 4 people
-# Only fellows can occupy living spaces
+
 class Living(Room):
 
-    # constructor
     def __init__(self):
         self.occupants = []
         self.max = 4
-
-    def getMax(self):
-    	""" Returns the maximum number of people allowed for instances of Living."""
-        return self.max
 
     def addFellow(self, fellow):
     	""" Adds a Fellow to a living space. Rejects Staff members."""
@@ -23,8 +17,5 @@ class Living(Room):
             else:
                 raise OverflowException('This living space is full!')
         else:
-            raise TypeError('Living spaces are meant for Fellows only!')
 
-    def getOccupants(self):
-    	""" Returns a list of current occupants for an instance of Living. """
-        return self.occupants
+            raise TypeError('Living spaces are meant for Fellows only!')
